e0c4959d4b18cd5d939ac7d54d32a00c443cc211
lujiang <531432594@qq.com>
2018-09-30 16:15:15 +0800

    hi

---
+++ hi.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lujiang
+ * Date: 2018/9/30
+ * Time: 16:06
+ */
+
+require_once './vendor/autoload.php';
+
+use Tools\Tools;
+
+echo Tools::hi();
---
+++ src/Tools/Tools.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lujiang
+ * Date: 2018/9/30
+ * Time: 15:57
+ */
+
+namespace Tools;
+
+class Tools
+{
+    public function __construct() {
+
+    }
+
+    public static function hi() {
+        echo 'hi';
+    }
+}
